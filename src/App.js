432c97cd1879a167eff078b048670e22f4febfcb
capta1nseal <aidan@lunesdale.community>
2025-01-31 16:26:41 +0200

    Change default page test.

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Kood sisu github clinic demo by<br></br>
+          <code>Aidan Walker</code>.
         </p>
         <a
           className="App-link"
